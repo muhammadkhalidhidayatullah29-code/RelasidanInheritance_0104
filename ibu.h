38b070ab168e1d27ccf0bcba4ab0e488f0bf2c20
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-05-12 18:57:15 +0700

    membuat akses public dengan member nama, pointer, constructor, destructor, dan fungction void

--- ibu.h
+++ ibu.h
@@ -6,6 +6,21 @@
 using namespace std;
 
 class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
 
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
 };
+
+
 #endif
