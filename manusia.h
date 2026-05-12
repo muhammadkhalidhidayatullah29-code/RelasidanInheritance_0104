03358e66919837800cd6b60e0247d3e3dec4b36a
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-05-12 19:24:01 +0700

    membuat class manusia dan menambah akses public dengan member properti variabel nama,  varJantung, constructor, dan destructor

--- manusia.h
+++ manusia.h
@@ -4,6 +4,18 @@
 #include <iostream>
 using namespace std;
 
+class manusia {
+public:
+    string name;
+    jantung varJantung;
 
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
 
 #endif
